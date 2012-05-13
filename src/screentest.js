5d15dd62819acb5fcc64c52760e9af7ae36c544f
Andrey Chizh <info@andreychizh.com>
2012-05-13 19:43:40 +0300

    ST-002 : Add bounds

--- src/screentest.js
+++ src/screentest.js
@@ -6,24 +6,95 @@
 
 var ScreenTest = function (bounds, href, browser, imgType) {
 
-
-    // Start test on window.onload event
-    // TODO: See cross-browsers onload
-    window.onload = function () {
-        init();
-    }
-
-    // Init function
     function init() {
-        drawImagesDOM();
+
+        // Start test on window.onload event
+        // TODO: See cross-browsers onload
+        window.onload = function () {
+            compareImages(bounds);
+
+        }
+
+        return result;
     }
 
-    function drawImagesDOM() {
-        var imagesSrc;
-        imagesSrc = getImagesSrc(href, browser, imgType);
-        createImageDOM('etalonImage', imagesSrc.etalon);
-        createImageDOM('testImage', imagesSrc.test);
+    function compareImages(bounds) {
+        var imagesData;
+        var imageDataLength;
+        var etalonImagePixel, testImagePixel;
+        var imageWidth;
+        var allPixels = 0,
+            errorPixels = 0;
 
+        imagesData = getImagesData();
+
+        etalonImagePixel = imagesData.etalon.data;
+        testImagePixel = imagesData.test.data;
+
+        imageDataLength = testImagePixel.length;
+        imageWidth = imagesData.width * 4;
+
+        for (var i = 0; i < imageDataLength; i += 4) {
+            if ((i > bounds[0] * 4 + imageWidth * parseInt(i / imageWidth)) &&
+                (i > bounds[1] * imageWidth) &&
+                (i < bounds[2] * 4 + imageWidth * parseInt(i / imageWidth)) &&
+                (i < bounds[3] * imageWidth)) {
+
+                allPixels += 1;
+
+                if (etalonImagePixel[i] != testImagePixel[i]) {
+                    testImagePixel[i] = 255;
+                    testImagePixel[i + 1] = 0;
+                    testImagePixel[i + 2] = 0;
+
+                    errorPixels += 1;
+                }
+            }
+        }
+
+        result = (errorPixels / allPixels) * 100;
+
+        imagesData.ctx.putImageData(imagesData.test, 0, 0);
+
+        alert(result);
+
+    }
+
+
+    function getImagesData() {
+        var imagesSrc;
+        var etalonImage, testImage;
+        var imageWidth, imageHeight;
+        var etalonCanvas, testCanvas;
+        var etalonCtx, testCtx;
+        var etalonImageData, testImageData;
+        var imagesData = {};
+
+        imagesSrc = getImagesSrc(href, browser, imgType);
+        etalonImage = createImageDOM('etalonImage', imagesSrc.etalon);
+        testImage = createImageDOM('testImage', imagesSrc.test);
+
+        imageWidth = testImage.width;
+        imageHeight = testImage.height;
+
+        etalonCanvas = createCanvasDOM('etalonCanvas', imageWidth, imageHeight);
+        testCanvas = createCanvasDOM('testCanvas', imageWidth, imageHeight);
+
+        etalonCtx = etalonCanvas.getContext('2d');
+        testCtx = testCanvas.getContext('2d');
+
+        etalonCtx.drawImage(etalonImage, 0, 0);
+        testCtx.drawImage(testImage, 0, 0);
+
+        etalonImageData = etalonCtx.getImageData(0, 0, imageWidth, imageHeight);
+        testImageData = testCtx.getImageData(0, 0, imageWidth, imageHeight);
+
+        imagesData.etalon = etalonImageData;
+        imagesData.test = testImageData;
+        imagesData.width = imageWidth;
+        imagesData.ctx = testCtx;
+
+        return imagesData;
     }
 
     function getImagesSrc(href, browser, imgType) {
@@ -41,16 +112,7 @@ var ScreenTest = function (bounds, href, browser, imgType) {
         img.id = id;
         img.src = src;
         body.appendChild(img);
-    }
-
-    // Get size of DOM element <img> with the specified id
-    function getSizeImageDOM(id) {
-        var img;
-        img = document.getElementById(id);
-        size = {};
-        size.width = img.width;
-        size.height = img.height;
-        return size;
+        return img;
     }
 
     // Delete DOM element <img> with the specified id
@@ -70,6 +132,7 @@ var ScreenTest = function (bounds, href, browser, imgType) {
         canvas.width = width;
         canvas.height = height;
         body.appendChild(canvas);
+        return canvas;
     }
 
     // Delete DOM element <canvas> with the specified id
@@ -81,17 +144,12 @@ var ScreenTest = function (bounds, href, browser, imgType) {
     }
 
 
-    this.getDiffPixels = function () {
-        deleteImgDOM('etalonImage');
-        return _bounds;
-    }
+    return init();
 
-    this.result = function () {
 
-        var test = true;
-        return test;
-    }
-
+    // this.result = function () {
+    //     return result;
+    // }
 
 
 }
